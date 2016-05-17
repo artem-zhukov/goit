9ac577806c892dcc00804a15676e477feb7e7040
Artem Zhukov <atem085@gmail.com>
2016-05-17 20:16:47 +0300

    js11-12

--- js11-12/js/script.js
+++ js11-12/js/script.js
@@ -1,55 +1,16 @@
 $(document).ready(function() {
-    var leftUIEl = $('.carousel-arrow-left');
-    var rightUIEl = $('.carousel-arrow-right');
-    var elementsList = $('.carousel-list');
 
-    var pixelsOffset = 125;
-    var currentLeftValue = 0;
-    var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
-    var maximumOffset = 0;
-
-    leftUIEl.click(function() {
-        if (currentLeftValue != maximumOffset) {
-          console.log("left");
-            currentLeftValue += 125;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
-
-    rightUIEl.click(function() {
-        //if (currentLeftValue != minimumOffset) {
-          console.log("right");
-            currentLeftValue -= 125;
-            elementsList.animate({left : currentLeftValue + "px"}, 500);
-      //  }
-    });
+$('.carousel-arrow-right').click(function() {
+    $(".carousel-list").animate({left: "-125px"}, 200);
+			$(".carousel-list .carousel-element").eq(0).clone().appendTo(".carousel-list");
+			$(".carousel-list .carousel-element").eq(0).remove();
+			$(".carousel-list").css({"left":"0px"});
 });
 
-
-// (function($){
-//   var leftUIEl = $('.slider-arrow-left');
-//   var rightUIEl = $('.slider-arrow-right');
-//   var elementsList = $('.slider-list');
-//
-//   var pixelsOffset = 125;
-//   var currentLeftValue = 0;
-//   var elementsCount = elementsList.find('li').length;
-//   var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
-//   var maximumOffset = 0;
-// $.fn.carousel = function(){
-//   console.log(this);
-//   console.log(rightUIEl);
-//   rightUIEl.click(function(){
-//       alert("!!!");
-//     if (currentLeftValue != minimumOffset) {
-//         currentLeftValue -= 125;
-//         elementsList.animate({ left : currentLeftValue + "px"}, 500);
-//     }
-//   return this;
-//   });
-// }
-// })(jQuery);
-//
-//
-// $('.carousel-arrow-right').carousel();
+$(".carousel-arrow-left").click(function(){
+		$(".carousel-list .carousel-element").eq(-1).clone().prependTo(".carousel-list");
+		$(".carousel-list").css({"left":"-125px"});
+		$(".carousel-list").animate({left: "0px"}, 300);
+		$(".carousel-list .carousel-element").eq(-1).remove();
+	});
+});
--- js11-12/js/tmplScript.js
+++ js11-12/js/tmplScript.js
@@ -1,20 +1,26 @@
 $(function() {
 
   var html = $('#page').html();
-  // var data = {
-  //   name: 'Жуков Артем Игоревич',
-  //   about:'Снова студент',
-  //   cause:'Хочу учить фронтенд, потому что:'
-  // };
-  var cause = [
+  var data = {
+    name: 'Жуков Артем Игоревич',
+    photo: 'img/photo.jpeg',
+    about:'Снова студент',
+    mainCause:'Хочу учить фронтенд, потому что:',
+    causes : [
     {item:'Познакомиться с интересными людьми'},
     {item:'Посмотреть мир'},
-    {item:'Фронтенд это интересно!!!'}
-  ];
+    {item:'Фронтенд это интересно!!!'}],
+    contacts : [
+      {item:'Мой контактный телефон'},
+      {item:'+380957414258'},
+      {item:'Мой профиль в контакте'},
+      {item:'http://goo.gl/rjrFhx'}],
+    motto :[
+      {item:'Мой фидбек:'},
+      {item: 'Не ленитесь! Да прибудет с Вами сила'}]
+}
 
-
-  var content = tmpl(html, cause);
+  var content = tmpl(html, {data:data});
   $('body').append(content);
-
 
 });
