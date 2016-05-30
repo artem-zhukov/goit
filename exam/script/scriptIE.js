839f96b67ef00c53c73c94081c50ff1213245f2a
Artem Zhukov <atem085@gmail.com>
2016-05-30 20:31:47 +0300

    v 0.1

---
+++ exam/script/scriptIE.js
@@ -0,0 +1,8 @@
+<!--[if lt IE 9]-->
+      document.createElement('header');
+      document.createElement('nav');
+      document.createElement('section');
+      document.createElement('article');
+      document.createElement('aside');
+      document.createElement('footer');
+<!--[endif]-->
